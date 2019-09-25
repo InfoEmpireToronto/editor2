13ab2337d5c2bd71f69e53019854b742aff42134
InfoEmpire Dev <git@infoempire.com>
2019-09-25 15:31:06 -0400

    asd

--- inc/head.php
+++ inc/head.php
@@ -28,10 +28,16 @@ if(!isset($_SESSION['UserData']['Username']) && $_SERVER['SCRIPT_NAME'] != $conf
   tinymce.init({
     selector: '#mytextarea',  // change this value according to your HTML
 	plugins: [
-	    'advlist autolink lists link image charmap print preview anchor',
+	    'advlist autolink lists link image imagetools charmap print preview anchor',
 	    'searchreplace visualblocks code fullscreen',
 	    'insertdatetime media table paste code help wordcount'
 	  ],
+	file_browser_callback: function(field_name, url, type, win) {
+	    // win.document.getElementById(field_name).value = 'my browser value';
+	    console.log('file browser callback', field_name, url, type, win);
+
+	},
+	images_upload_url: 'postAcceptor.php',
 	// menubar: "insert",
 	// toolbar: "media"
   });
