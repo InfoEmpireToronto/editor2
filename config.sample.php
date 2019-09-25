b7e5bb16de90df685979c064db343d94cda937d6
InfoEmpire Dev <git@infoempire.com>
2019-09-25 13:23:22 -0400

    asd

--- config.sample.php
+++ config.sample.php
@@ -1,17 +1,28 @@
 <?php
-$config['AppLocation'] = '/';
-$config['AppURL'] = 'localhost'.$config['AppLocation'];
+$config['DBUG'] = TRUE;
 
 $config['dbServer'] = 'localhost';
 $config['db'] = 'editor2';
 $config['dbUser'] = '';
 $config['dbPass'] = '';
 
-$config['client'] = '';
+$config['importOnLaunch'] = FALSE;
+$config['client'] = 0;
 
 $config['enable_categories'] = '1';
 
+$config['AppLocation'] = '/';
+$config['AppURL'] = 'http://****'.$config['AppLocation'];
+
 $config['login'] = 'admin';
 $config['password'] = 'admin';
+
+
+if($config['DBUG'])
+{
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+}
 
 ?>
