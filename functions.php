3b8df275b0c081ef61a439d4c517a3bf84fa8ed1
InfoEmpire Dev <git@infoempire.com>
2019-09-26 12:07:37 -0400

    asd

--- functions.php
+++ functions.php
@@ -284,8 +284,7 @@ class Article
 		$start = 0;
 		$length = 0;
 		$image = $this->getImage($start, $length);
-		$this->data['body'] = substr_replace($this->data['body'], '', $start, $length);
-		dd([$this->data['body'], '', $start, $length]);
+		$this->data['body'] = $this->content = substr_replace($this->data['body'], '', $start, $length);
 		return $image;
 	}
 	function getSummary($chars = 100)
