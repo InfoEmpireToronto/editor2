517103da6ef7c8eecef874e9528ce4b810bea76f
InfoEmpire Dev <git@infoempire.com>
2019-10-01 13:53:18 -0400

    asd

--- functions.php
+++ functions.php
@@ -446,12 +446,12 @@ class Article
 
 				$title = urldecode(substr($url->loc, $start, ($end-$start)));
 				$id = urldecode(substr($url->loc, $end+1, 7));
-				$newURL = $newData[$id]->getURL($this->config['articlePrefix']);
+				// $newURL = $newData[$id]->getURL($this->config['articlePrefix']);
 
 				dump($id);
 				dump($title);
 				dump($url);
-				dump($newURL);
+				// dump($newURL);
 			}
 
 
